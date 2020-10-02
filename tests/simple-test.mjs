d1ef337dd4cedc462dae81493de48aa5171b9daa
Markus Felten <Markus.Felten@db.com>
2020-10-02 14:04:01 +0200

    test: mock Reader

--- tests/simple-test.mjs
+++ tests/simple-test.mjs
@@ -1,10 +1,28 @@
 import test from "ava";
 import { lineIterator } from "reader-line-iterator";
 
-test("1st.", async t => {
-  const reader = new Reader();
-  for await (const line of lineIterator(reader)) {
-    console.log(line);
+class Reader {
+  constructor(chunks)
+  {
+     this.chunks = chunks;
   }
 
+  async * read()
+  {
+     for(const chunk of this.chunks) {
+       yield { value: chunk, done: false};
+     }
+     yield { value: undefined, done: true };
+  }
+}
+
+test("1st.", async t => {
+  const reader = new Reader([""]);
+  const lines = [];
+
+  for await (const line of lineIterator(reader)) {
+    lines.push(line);
+  }
+
+  t.deepEqual(lines,[]);
 }); 
